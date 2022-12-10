5f81681ebd5c9343043cb95be8b002532a438fad
unknown <bensonagala@live.com>
2022-12-10 15:12:10 +0300

    portfolio setup

--- app.vue
+++ app.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="flex justify-center items-center h-screen">
-    <h1 class="text-3xl font-bold text-green-500">
-      How to install Tailwind CSS 3 in Nuxt 3
-    </h1>
+  <div>
+    Hello nuxt 3
+    <pre>{{ mountains }}</pre>
+
   </div>
 </template>
+
+<script>
+import { AsyncData } from 'nuxt/app/composables';
+
+  export default {
+    async setup(props) {
+      const AsyncData = useAsyncData()
+      const {data} = await AsyncData(() => {
+        return $fetch('https://api.nuxtjs.dev/mountains')
+      })
+      return{ mountains }
+    }
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
