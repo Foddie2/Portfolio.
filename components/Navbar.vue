7eb3456b395bcf1e3158cae82492cc7bc08ed4f7
unknown <bensonagala@live.com>
2022-12-14 09:25:32 +0300

    portfolio setup

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,77 +1,76 @@
 <template>
-    <div class="min-h-screen bg-white">
-    <nav class="flex px-4 border-b md:shadow-lg items-center relative">
-        <div class="text-lg font-bold md:py-0 py-4">
-            Logo
+    <div class="bg-indigo-600">
+      <nav
+        class="
+          container
+          px-6
+          py-8
+          mx-auto
+          md:flex md:justify-between md:items-center
+        "
+      >
+        <div class="flex items-center justify-between">
+          <router-link
+            to="/"
+            class="
+              text-xl
+              font-bold
+              text-gray-100
+              md:text-2xl
+              hover:text-indigo-400
+            "
+            >Logo
+          </router-link>
+          <!-- Mobile menu button -->
+          <div @click="toggleNav" class="flex md:hidden">
+            <button
+              type="button"
+              class="
+                text-gray-100
+                hover:text-gray-400
+                focus:outline-none focus:text-gray-400
+              "
+            >
+              <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                <path
+                  fill-rule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                ></path>
+              </svg>
+            </button>
+          </div>
         </div>
-        <ul class="md:px-2 ml-auto md:flex md:space-x-2 absolute md:relative top-full left-0 right-0">
-            <li>
-                <a href="#" class="flex md:inline-flex p-4 items-center hover:bg-gray-50">
-                    <span>Home</span>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="flex md:inline-flex p-4 items-center hover:bg-gray-50">
-                    <span>Products</span>
-                </a>
-            </li>
-            <li class="relative parent">
-                <a href="#" class="flex justify-between md:inline-flex p-4 items-center hover:bg-gray-50 space-x-2">
-                    <span>Service</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-current pt-1" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg>
-                </a>
-                <ul class="child transition duration-300 md:absolute top-full right-0 md:w-48 bg-white md:shadow-lg md:rounded-b ">
-                    <li>
-                        <a href="#" class="flex px-4 py-3 hover:bg-gray-50">
-                            Web development
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="flex px-4 py-3 hover:bg-gray-50">
-                            Web Design
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="flex px-4 py-3 hover:bg-gray-50">
-                            Machine Learning
-                        </a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="#" class="flex md:inline-flex p-4 items-center hover:bg-gray-50">
-                    <span>About us</span>
-                </a>
-            </li>
+  
+        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+        <ul
+          :class="showMenu ? 'flex' : 'hidden'"
+          class="
+            flex-col
+            mt-8
+            space-y-4
+            md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0
+          "
+        >
+          <li class="text-gray-100 hover:text-indigo-400">Home</li>
+          <li class="text-gray-100 hover:text-indigo-400">About</li>
+          <li class="text-gray-100 hover:text-indigo-400">Blogs</li>
+          <li class="text-gray-100 hover:text-indigo-400">Contact Us</li>
         </ul>
-        <div class="ml-auto md:hidden text-gray-500 cursor-pointer">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 fill-current" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
-        </div>
-    </nav>
+      </nav>
     </div>
-
   </template>
 
 <script>
     export default {
+        setup() {
+    let showMenu = ref(false);
+    const toggleNav = () => (showMenu.value = !showMenu.value);
+    return { showMenu, toggleNav };
+  },
         
     }
 </script>
 
 <style scoped>
-@media only screen and (min-width: 768px){
-        .parent:hover .child {
-            opacity:1;
-            height:auto;
-            overflow:none;
-            transform: translateY(0);
-        }
-        .child {
-            opacity:0;
-            height:0;
-            overflow:hidden;
-            transform: translateY(-10%);
-        }
-    }
 
 </style>
