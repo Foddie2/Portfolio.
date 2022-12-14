3e4f87adec5388f13c1d26633cd6c859276f09fb
unknown <bensonagala@live.com>
2022-12-14 10:44:38 +0300

    portfolio setup

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-white    ">
+    <div class="bg-white">
       <nav
         class="
           container
@@ -51,10 +51,42 @@
             md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0
           "
         >
-          <li class="text-gray-700 hover:text-indigo-400">Home</li>
-          <li class="text-gray-700 hover:text-indigo-400">About</li>
-          <li class="text-gray-700 hover:text-indigo-400">Blogs</li>
-          <li class="text-gray-700 hover:text-indigo-400">Contact Us</li>
+          <li class="text-gray-700 hover:text-indigo-400">
+            <a href="#" class="flex justify-between md:inline-flex p-4 items-center hover:bg-gray-50 space-x-2">
+                    <span>Home</span>
+                </a>
+          </li>
+          <li class="text-gray-700 hover:text-indigo-400">
+            <a href="#" class="flex justify-between md:inline-flex p-4 items-center hover:bg-gray-50 space-x-2">
+                    <span>About</span>
+                </a>
+          </li>
+
+
+          <li class="relative parent text-gray-700 hover:text-indigo-400">
+            <a href="#" class="flex justify-between md:inline-flex p-4 items-center hover:bg-gray-50 space-x-2">
+                    <span>Service</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-current pt-1" viewBox="0 0 24 24"><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/></svg>
+                </a>
+            <ul class="child transition duration-300 md:absolute top-full right-0 md:w-48 bg-white md:shadow-lg md:rounded-b ">
+                    <li>
+                        <a href="#" class="flex px-4 py-3 hover:bg-gray-50">
+                            Web development
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="flex px-4 py-3 hover:bg-gray-50">
+                            Web Design
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="flex px-4 py-3 hover:bg-gray-50">
+                            Machine Learning
+                        </a>
+                    </li>
+                </ul>
+          </li>
+          <li class="text-gray-700 hover:text-indigo-400">Contact</li>
         </ul>
       </nav>
     </div>
@@ -63,14 +95,28 @@
 <script>
     export default {
         setup() {
-    let showMenu = ref(false);
-    const toggleNav = () => (showMenu.value = !showMenu.value);
-    return { showMenu, toggleNav };
-  },
-        
+            let showMenu = ref(false);
+            const toggleNav = () => (showMenu.value = !showMenu.value);
+            return { showMenu, toggleNav };
+        },     
     }
 </script>
 
 <style scoped>
+    
+@media only screen and (min-width: 768px){
+        .parent:hover .child {
+            opacity:1;
+            height:auto;
+            overflow:none       ;
+            transform: translateY(0);
+        }
+        .child {
+            opacity:0;
+            height:0;
+            overflow:hidden;
+            transform: translateY(-10%);
+        }
+    }
 
 </style>
