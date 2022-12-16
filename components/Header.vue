16f120574a094d5f8cb01e502e55bfa3a63d3ebc
unknown <bensonagala@live.com>
2022-12-16 13:41:31 +0300

    portfolio setup

--- components/Header.vue
+++ components/Header.vue
@@ -4,14 +4,14 @@
 	<div class="bg-fuchsia-100 text-gray-500 py-20">
 		<div class="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
 			<div class="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
-				<h1 class="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">TechIT</h1>
+				<h1 class="text-3xl md:text-5xl p-2 text-rose-600 tracking-loose">TechIT</h1>
 				<h2 class="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">Space : The Timeless Infinity
 				</h2>
 				<p class="text-sm md:text-base text-gray-500 mb-4">Explore your favourite events and
 					register now to showcase your talent and win exciting prizes.</p>
 				<a href="#"
-					class="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent">
-					Explore Now</a>
+					class="bg-transparent hover:bg-rose-600 text-rose-600 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-rose-600 hover:border-transparent">
+					Connect</a>
 			</div>
 			<div class="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
 				<div class="h-48 flex flex-wrap content-center">
