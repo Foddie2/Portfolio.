25649ad759a528ad55ce15e96520281d9c50c1cb
unknown <bensonagala@live.com>
2022-12-17 15:08:36 +0300

    portfolio setup

--- app.vue
+++ app.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <Navbar/>
-    <Header/>
-    <About/>
-    <Footer/>
+    <NuxtPage>
+    
+    </NuxtPage>
 
   </div>
 </template>
 
 <script>
-
+  export default {
+    
+  }
 </script>
 
 <style lang="scss" scoped>
