125cfad201ddf5dbef881e17ceb4eee5471f3ad7
unknown <bensonagala@live.com>
2022-12-20 15:21:41 +0300

    portfolio setup

--- components/About.vue
+++ components/About.vue
@@ -1,19 +1,64 @@
 <template>
-
-<div >
-
-</div>
-
-
-
+	<div class=" text-gray-700 pt-12 pb-12 bg-violet-50">
+	  <div class="text-center md:max-w-xl lg:max-w-3xl mx-auto mt-10 mb-10">
+		<h2 class="text-3xl font-bold mb-6 text-gray-700 uppercase">Event Planning & Design</h2>
+		<p class="mb-6 pb-2 md:mb-12 md:pb-0 text-base text-center text-gray-500">
+		  Wasali Events is a premier, full-service event planning and design company specializing in distinctive weddings, luxurious events, non-profit fund-raisers, and corporate functions.We have the perfect combination of sophistication, style, and creative enthusiasm to make your celebration a success. <br/><br/> Our expertise lies in being able to see the big picture without forgoing the details - ultimately creating events that our clients and their guests remember for years.    Highly sought after and in demand we have traveled the world to bring together our clients' visions.
+		</p>
+		<NuxtLink to="/AboutUs">
+		<button
+		  class="wobble-vertical bg-transparent hover:bg-purple-500 text-purple font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded text-gray-700">
+		  FIND OUT MORE
+		</button>
+		</NuxtLink>
+		</div>
+	  </div>
   </template>
-
-<script>
-    export default {
-        
-    }
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  
+  <script>
+  
+  </script>
+  
+  <style >
+  /* Wobble Vertical */
+	@keyframes wobble-vertical {
+	  16.65% {
+		transform: translateY(8px);
+	  }
+  
+	  33.3% {
+		transform: translateY(-6px);
+	  }
+  
+	  49.95% {
+		transform: translateY(4px);
+	  }
+  
+	  66.6% {
+		transform: translateY(-2px);
+	  }
+  
+	  83.25% {
+		transform: translateY(1px);
+	  }
+  
+	  100% {
+		transform: translateY(0);
+	  }
+	}
+  
+  .wobble-vertical {
+	display: inline-block;
+	-webkit-tap-highlight-color: transparent;
+	transform: translateZ(0);
+	box-shadow: 0 0 1px transparent;
+  }
+  
+  .wobble-vertical:hover {
+	animation-name: wobble-vertical;
+	animation-duration: 1s;
+	animation-timing-function: ease-in-out;
+	animation-iteration-count: 1;
+  }
+  </style>
+  
