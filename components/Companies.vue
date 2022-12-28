9398a2de3544d46b09ea325c2a289a22928f6a93
unknown <bensonagala@live.com>
2022-12-28 21:53:04 +0300

    portfolio setup

--- components/Companies.vue
+++ components/Companies.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="bg-violet-50 px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+  <section class="bg-violet-50">
+    <div class=" px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
     <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
       <!-- <div>
         <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
@@ -18,7 +19,7 @@
           </svg>
           <span class="relative">The</span>
         </span>
-        quick, brown fox jumps over a lazy dog
+        Companies worked for
       </h2>
       <p class="text-base text-gray-700 md:text-lg">
         Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.
@@ -82,6 +83,7 @@
     </div>
     </div>
   </div>
+</section>
 </template>
 
 <script>
