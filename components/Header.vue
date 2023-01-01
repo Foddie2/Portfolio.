805b428ac77df2f4fd3c948fbc3e42ae0b7c4461
unknown <bensonagala@live.com>
2023-01-01 15:12:01 +0300

    portfolio setup

--- components/Header.vue
+++ components/Header.vue
@@ -18,7 +18,7 @@
                 <div class="absolute inset-0 bg-violet-500 hidden md:block transform md:translate-y-2 md:translate-x-4 xl:translate-y-4 xl:translate-x-8 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out pointer-events-none" aria-hidden="true"></div>
                 <figure class="relative h-0 pb-[56.25%] md:pb-[75%] lg:pb-[56.25%] overflow-hidden transform md:-translate-y-2 xl:-translate-y-4 group-hover:translate-x-0 group-hover:translate-y-0 transition duration-700 ease-out">
                     <img class="absolute inset-0 w-full h-full object-cover transform hover:scale-105 transition duration-700 ease-out" 
-                        src="https://preview.cruip.com/open-pro/images/blog-post-01.jpg" width="540" height="303" alt="Blog post">
+                        src="https://preview.cruip.com/open-pro/images/blog-post-01.jpg" width="540" height="303" alt="My-Image">
                 </figure>
             </a>
         </article>    
