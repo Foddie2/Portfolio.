ca3a47be62eaaa0dc6989d9a5d792364cac653c8
unknown <bensonagala@live.com>
2023-01-03 22:08:36 +0300

    portfolio setup

--- components/Contact.vue
+++ components/Contact.vue
@@ -39,7 +39,7 @@
             </svg>
             <span> 0711974716 </span>
           </p>
-          <!--
+          
           <p class="flex items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
@@ -47,8 +47,8 @@
               <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
               <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
             </svg>
-            <span>contact@business.com</span>
-          </p> -->
+            <span>bensonagala@live.com</span>
+          </p>
         </div>
       </div>
       <form  novalidate="" class="flex flex-col py-6 space-y-6 md:py-0 md:px-6 ng-untouched ng-pristine ng-valid">
