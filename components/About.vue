37fc4e91b18d5ed97397646bdca943493839f17b
unknown <bensonagala@live.com>
2023-01-07 21:24:15 +0300

    portfolio setup

--- components/About.vue
+++ components/About.vue
@@ -3,7 +3,12 @@
 	  <div class="text-center lg:max-w-3xl mx-auto mt-10 mb-10  max-w-6xl p-8 sm:px-6">
 		<h2 class="text-3xl font-bold mb-6 text-gray-700">About Me</h2>
 		<p class="mb-6 pb-2 md:mb-12 md:pb-0 text-base text-center text-gray-500">
-		  Wasali Events is a premier, full-service event planning and design company specializing in distinctive weddings, luxurious events, non-profit fund-raisers, and corporate functions.We have the perfect combination of sophistication, style, and creative enthusiasm to make your celebration a success. <br/><br/> Our expertise lies in being able to see the big picture without forgoing the details - ultimately creating events that our clients and their guests remember for years.    Highly sought after and in demand we have traveled the world to bring together our clients' visions.
+Experienced Web developer with experience in both front-end and back-end development with a passion for crafting intuitive and 
+user-friendly web applications. Proficient in a variety of programming languages and development frameworks. Skilled in HTML, CSS, 
+JavaScript, and PHP. Passionate about web standards and creating user-friendly websites. Proven ability in designing and developing 
+user interfaces, testing, debugging, and training staff within ECommerce technologies, and optimizing web functionality that improve 
+data retrieval and workflow efficiencies.
+
 		</p>
 		</div>
 	  </section>
