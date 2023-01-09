175e982450249a4a3ccf0675bc0ebcea637b7d21
unknown <bensonagala@live.com>
2023-01-09 17:34:18 +0300

    portfolio setup

--- components/About.vue
+++ components/About.vue
@@ -3,12 +3,11 @@
 	  <div class="text-center lg:max-w-3xl mx-auto mt-10 mb-10  max-w-6xl p-8 sm:px-6">
 		<h2 class="text-3xl font-bold mb-6 text-gray-700">About Me</h2>
 		<p class="mb-6 pb-2 md:mb-12 md:pb-0 text-base text-center text-gray-500">
-Experienced Web developer with experience in both front-end and back-end development with a passion for crafting intuitive and 
-user-friendly web applications. Proficient in a variety of programming languages and development frameworks. Skilled in HTML, CSS, 
-JavaScript, and PHP. Passionate about web standards and creating user-friendly websites. Proven ability in designing and developing 
-user interfaces, testing, debugging, and training staff within ECommerce technologies, and optimizing web functionality that improve 
-data retrieval and workflow efficiencies.
-
+			Experienced Web developer with experience in both front-end and back-end development with a passion for crafting intuitive and 
+			user-friendly web applications. Proficient in a variety of programming languages and development frameworks. Skilled in HTML, CSS, 
+			JavaScript, and PHP. Passionate about web standards and creating user-friendly websites. Proven ability in designing and developing 
+			user interfaces, testing, debugging, and training staff within ECommerce technologies, and optimizing web functionality that improve 
+			data retrieval and workflow efficiencies.
 		</p>
 		</div>
 	  </section>
