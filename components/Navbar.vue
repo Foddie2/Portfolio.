63d237f87a5546cb3a5deb75cb068f5b7a1242de
Foddie2 <Bensonagala@live.com>
2023-01-12 19:10:42 +0300

    bug fix

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -37,12 +37,12 @@
         <ul :class="showMenu ? 'flex' : 'hidden'"
           class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
           <li class="text-gray-500 hover:text-blue-500 font-medium">
-            <a href="#About" class="link link-underline link-underline-black flex justify-between md:inline-flex p-4 items-center hover:bg-violet-50 space-x-2">
+            <a href="#About" class="link link-underline link-underline-black flex justify-between md:inline-flex p-4 items-center space-x-2">
               <span>About</span>
             </a>
           </li>
           <li class="text-gray-500 hover:text-blue-500 font-medium">
-            <a href="#Portfolio" class="link link-underline link-underline-black flex justify-between md:inline-flex p-4 items-center hover:bg-violet-50 space-x-2">
+            <a href="#Portfolio" class="link link-underline link-underline-black flex justify-between md:inline-flex p-4 items-center space-x-2">
               <span>Portfolio </span>
             </a>
           </li>
@@ -73,7 +73,7 @@
                 </ul>
           </li>
           <li class="text-gray-500 hover:text-blue-500 font-medium">
-            <a href="#Contact" class="link link-underline link-underline-black flex justify-between md:inline-flex p-4 items-center hover:bg-violet-50 space-x-2">
+            <a href="#Contact" class="link link-underline link-underline-black flex justify-between md:inline-flex p-4 items-center space-x-2">
               <span>Contact</span>
             </a>
           </li>
